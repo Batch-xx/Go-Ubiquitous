7dd2d6cfdedabe38e8ad5879c27f35b6dea72fc4
Brian Batchelor <batchbk@gmail.com>
2016-09-28 16:42:57 -0400

    Feature: Creating WatchFace Create a method that send a temperature request thru MessageAPI to the moblie device

--- Advanced_Android_Development/wearable/src/main/java/com/example/android/sunshine/app/SyncJobService.java
+++ Advanced_Android_Development/wearable/src/main/java/com/example/android/sunshine/app/SyncJobService.java
@@ -2,18 +2,65 @@ package com.example.android.sunshine.app;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.MessageApi;
+import com.google.android.gms.wearable.Wearable;
 
 /**
  * Created by BKBachelor on 9/28/2016.
  */
 
 public class SyncJobService extends JobService {
+    public static int SYNC_TEMP_ID = 1;
+    private static String SYNC_TEMP_REQUEST = "/sync_temp_request";
+    private GoogleApiClient mGoogleClientAPI;
     private String TAG = "SyncJobService";
+
+    @Override
+    public void onCreate() {
+        mGoogleClientAPI = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(@Nullable Bundle bundle) {
+                        Log.d(TAG, "Google Client API CONNECTED");
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+                        Log.d(TAG, "Google Client API SUSPENDED");
+                    }
+                })
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Log.e(TAG, "Google Client API FAILED");
+                    }
+                })
+                .addApi(Wearable.API)
+                .build();
+        super.onCreate();
+    }
+
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
         Log.d(TAG,"SyncJobService has started...");
         Log.d(TAG, "Job ID: " + jobParameters.getJobId() + " STARTED");
+
+        int job_id = jobParameters.getJobId();
+        switch (job_id){
+            case 1:
+                requestTemperature();
+                break;
+            default:
+                Log.e(TAG, "Invalid job ID received....");
+        }
         return false;
     }
 
@@ -23,4 +70,20 @@ public class SyncJobService extends JobService {
         Log.d(TAG, "Job ID: " + jobParameters.getJobId() + " STOPPED");
         return false;
     }
+
+
+
+    private void requestTemperature(){
+        Log.d(TAG, "Requesting Temperature....");
+        Wearable.MessageApi.sendMessage(mGoogleClientAPI, String.valueOf(SYNC_TEMP_ID),
+                SYNC_TEMP_REQUEST,null).setResultCallback(new ResultCallback<MessageApi.SendMessageResult>() {
+            @Override
+            public void onResult(@NonNull MessageApi.SendMessageResult sendMessageResult) {
+                if(!sendMessageResult.getStatus().isSuccess()){
+                    Log.e(TAG, "FAILED to send message ID: " + SYNC_TEMP_ID);
+                }
+            }
+        });
+    }
+
 }
--- Advanced_Android_Development/wearable/src/main/java/com/example/android/sunshine/app/WeatherWatchFace.java
+++ Advanced_Android_Development/wearable/src/main/java/com/example/android/sunshine/app/WeatherWatchFace.java
@@ -32,12 +32,18 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.wearable.watchface.CanvasWatchFaceService;
 import android.support.wearable.watchface.WatchFaceStyle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.WindowInsets;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.Wearable;
 
 import java.lang.ref.WeakReference;
 import java.util.Calendar;
@@ -63,9 +69,16 @@ public class WeatherWatchFace extends CanvasWatchFaceService {
      */
     private static final int MSG_UPDATE_TIME = 0;
 
-    private final int SYNC_JOB_ID = 1;
 
     private String TAG = "WeatherWatchFace";
+
+    private GoogleApiClient mGoogleClientAPI;
+
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+    }
 
     @Override
     public Engine onCreateEngine() {
@@ -138,7 +151,7 @@ public class WeatherWatchFace extends CanvasWatchFaceService {
 
             // Job Temp Sync Scheduler
             ComponentName jobSvcName = new ComponentName(WeatherWatchFace.this,SyncJobService.class);
-            JobInfo syncTempJob = new JobInfo.Builder(SYNC_JOB_ID,jobSvcName)
+            JobInfo syncTempJob = new JobInfo.Builder(SyncJobService.SYNC_TEMP_ID,jobSvcName)
                     .setPeriodic((long)resources.getInteger(R.integer.syncTimeInterval))
                     .setRequiresDeviceIdle(true)
                     .build();
